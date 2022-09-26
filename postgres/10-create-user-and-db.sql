26755a12d55ea3ed52b4cab521bd0a9ccb2544af
Alexey Romanov <avromanov@sberdevices.ru>
2022-09-26 18:55:57 +0300

    db: introduce postgres schema

--- postgres/10-create-user-and-db.sql
+++ postgres/10-create-user-and-db.sql
@@ -3,3 +3,19 @@ CREATE DATABASE persons;
 CREATE ROLE program WITH PASSWORD 'test';
 GRANT ALL PRIVILEGES ON DATABASE persons TO program;
 ALTER ROLE program WITH LOGIN;
+
+\c persons;
+
+CREATE TABLE persons (
+    id serial PRIMARY KEY,
+    name VARCHAR (64),
+    age INT,
+    address VARCHAR(128),
+    work VARCHAR(128)
+);
+
+INSERT INTO persons(name, age, address, work) VALUES
+    ('Dmitry Yakuba', 22, 'Yasenevo', 'IVI'),
+    ('Pavel Perestoronin', 22, 'Yasenevo', 'Yandex'),
+    ('Mikhail Nitenko', 22, 'Mauritius', 'Parallels'),
+    ('Sergey Kononenko', 22, 'Astana', 'SberMegaMarket');
